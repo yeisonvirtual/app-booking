53ef1fb963cfcb18dc3a3c7e7f8f4b1c67026675
Yeison Rojas <yeisonjr98@gmail.com>
2024-03-13 08:33:38 -0400

    fix env

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,10 @@ const nextConfig = {
     domains: ['localhost', 'https://api-booking-105b.onrender.com'],
   },
   env: {
-    // API_URL: 'https://api-booking-105b.onrender.com',
-    // FRONT_URL: 'https://app-booking-rho.vercel.app',
-    API_URL: 'http://localhost:8080',
-    FRONT_URL: 'http://localhost:3000',
+    API_URL: 'https://api-booking-105b.onrender.com',
+    FRONT_URL: 'https://app-booking-rho.vercel.app',
+    // API_URL: 'http://localhost:8080',
+    // FRONT_URL: 'http://localhost:3000',
     SECRET: 'secret_key'
   }
 };
